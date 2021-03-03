03a1a4a25abec20040c46a3e6baa36d1b6c1ccc3
Brandon <bfgithub@gmail.com>
2021-03-03 21:54:43 +0000

    Working Student Networks Creation

---
+++ main.tf
@@ -0,0 +1,5 @@
+provider "google" {
+  project = "{{YOUR GCP PROJECT}}"
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "numberOfStudents" {
+  default = 20
+}
+variable "project_id" {
+  type        = string
+  default = "f5-gcs-4261-sales-agility2021"
+  description = <<EOD
+The GCP project identifier to use for testing.
+EOD
+}
+variable "region" {
+  type        = string
+  default     = "us-west1"
+  description = <<EOD
+The region to deploy test resources. Default is 'us-west1'.
+EOD
+}
+variable "admin_source_cidrs" {
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+  description = <<EOD
+The list of source CIDRs that will be added to firewall rules to allow admin
+access to BIG-IPs (SSH and GUI) on alpha and beta subnetworks. Only useful if
+instance has a public IP address.
+EOD
+}
