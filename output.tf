43e7c4e4d85a186d9f9a5882c20f63c4567884b7
el-coder <ezmarte@gmail.com>
2021-03-15 20:10:17 -0400

    added output file

---
+++ output.tf
@@ -0,0 +1,16 @@
+output bigip_1_mgmtIP {
+  value = module.bigip_1.mgmtPublicIP
+  
+}
+output bigip_2_mgmtIP {
+  value = module.bigip_2.mgmtPublicIP
+  
+}
+output bigip_username {
+  value = module.bigip_2.f5_username
+  
+}
+output bigip_password {
+  value = module.bigip_2.bigip_password
+  
+}
