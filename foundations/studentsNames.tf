3ec185428e78d63bbc4564bcf7f1c48729da449f
snowblind- <snowblind-@users.noreply.github.com>
2021-04-07 11:27:54 -0700

    Update studentsNames.tf

--- foundations/studentsNames.tf
+++ foundations/studentsNames.tf
@@ -1,5 +1,10 @@
+variable "listOfNames" {
+  type = list(string)
+}
+
 resource "google_project_iam_member" "project" {
-  project = var.project_id
-  role    = "roles/editor"
-  member  = "user:bfgithub@gmail.com"
+  for_each = toset(var.listOfNames)
+    project = var.project_id
+    role    = "roles/editor"
+    member  = format("user:%s",each.value)
 }
