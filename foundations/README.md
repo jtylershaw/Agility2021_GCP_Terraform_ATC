4a48405f193d6e18eaa735ab334b9eb63f87623b
snowblind- <snowblind-@users.noreply.github.com>
2021-04-16 12:30:03 -0700

    added output list

--- foundations/README.md
+++ foundations/README.md
@@ -6,3 +6,5 @@ it is not intended to be executed by Agility lab administrators only.
 Terraform cloud expects a variable to be created which is a list of email addresses for IAM role updates
 
 listOfNames HCL ["bf*****@gmail.com","jt*****@gmail.com","ph*****@@gmail.com"]
+
+expirationTime 
--- foundations/studentsNames.tf
+++ foundations/studentsNames.tf
@@ -2,11 +2,18 @@ variable "listOfNames" {
   type = list(string)
 }
 
+variable "expiration"
+
 resource "google_project_iam_member" "editors" {
   for_each = toset(var.listOfNames)
     project = var.project_id
     role    = "roles/editor"
     member  = format("user:%s",each.value)
+
+  condition {
+    title       = "studentEditorExpiration"
+    expression  = "request.time < timestamp(\"2021-16-04T00:00:00Z\")"
+  }
 }
 
 resource "google_project_iam_member" "iamAdmin" {
@@ -14,4 +21,14 @@ resource "google_project_iam_member" "iamAdmin" {
     project = var.project_id
     role    = "roles/iam.roleAdmin"
     member  = format("user:%s",each.value)
+
+  condition {
+    title       = "studentIAMexpiration"
+    expression  = "request.time < timestamp(\"2021-16-04T00:00:00Z\")"
+  }
+}
+
+output "studentIDemail" {
+  for_each = toset(var.listOfNames)
+    value = format("user:%s student:%s",each.value, index(listOfNames, value))
 }
